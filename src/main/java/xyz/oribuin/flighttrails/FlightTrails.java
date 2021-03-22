fccb8fd4ff8be03291d788c4ef5f73d31e720b34
Oribuin <rjmullineux@gmail.com>
2021-03-22 18:56:37 +0000

    delete *everything*

---
+++ src/main/java/xyz/oribuin/flighttrails/FlightTrails.java
@@ -0,0 +1,15 @@
+package xyz.oribuin.flighttrails;
+
+import xyz.oribuin.orilibrary.OriPlugin;
+
+public class FlightTrails extends OriPlugin {
+    @Override
+    public void enablePlugin() {
+
+    }
+
+    @Override
+    public void disablePlugin() {
+
+    }
+}
