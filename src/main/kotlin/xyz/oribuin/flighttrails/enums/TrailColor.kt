5d52cfb2f4805c617202011c9ad30ed7ed43b28c
Oribuin <rjmullineux@gmail.com>
2021-03-30 02:36:26 +0100

    Convert org.bukkit.Color into enums

---
+++ src/main/kotlin/xyz/oribuin/flighttrails/enums/TrailColor.kt
@@ -0,0 +1,22 @@
+package xyz.oribuin.flighttrails.enums
+
+import org.bukkit.Color
+
+enum class TrailColor(val color: Color) {
+    WHITE(Color.WHITE),
+    LIGHT_GRAY(Color.SILVER),
+    GRAY(Color.GRAY),
+    BLACK(Color.BLACK),
+    RED(Color.RED),
+    DARK_RED(Color.MAROON),
+    YELLOW(Color.YELLOW),
+    LIME(Color.LIME),
+    GREEN(Color.GREEN),
+    AQUA(Color.AQUA),
+    TEAL(Color.TEAL),
+    BLUE(Color.BLUE),
+    NAVY(Color.NAVY),
+    PURPLE(Color.FUCHSIA),
+    PINK(Color.PURPLE),
+    ORANGE(Color.ORANGE)
+}
